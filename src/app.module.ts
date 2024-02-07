c2bc70e0f7d69bcf65ccdc0ffa047ed67498456c
김명식 <rlaaudtlr233@naver.com>
2024-02-07 18:11:19 +0900

    [feat] 몽고디비 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // app 모듈에서만 config모듈을 임폴트 시키면 다른 모듈에서도 환경변수를 사용할 수 있게해주는 옵션
     }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
   ],
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+// 미들웨어를 사용하기위해 미들웨어를 등록
+export class AppModule implements NestModule {
+  configure() {
+    const DEBUG = process.env.MODE === 'dev' ? true : false;
+    // 몽구스 쿼리가 로그로 찍히게 하기 위해
+    mongoose.set('debug', DEBUG);
+  }
+}
