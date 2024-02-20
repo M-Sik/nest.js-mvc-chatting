10840c42b7d03a669e61fa092b19f51833118508
김명식 <rlaaudtlr233@naver.com>
2024-02-20 16:56:18 +0900

    클라이언트, 서버간 소켓 emit, on 메서드를 통해 통신하는 공부

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,6 +7,13 @@ const formElement = getElementById('chat_form');
 
 function helloUser() {
   const username = prompt('What is your name');
+
+  // emit은 보낼때 사용하는 메서드
+  soket.emit('new_user', username);
+  // on은 받을때 사용하는 메서드
+  soket.on('hello_user', (data) => {
+    console.log(data);
+  });
 }
 
 function init() {
--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,9 +1,19 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'dgram';
 
 @WebSocketGateway()
 export class ChatsGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  @SubscribeMessage('new_user') // 소켓을 통해 주고받을 이벤트명을 기입
+  handleNewUser(
+    @MessageBody() username: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    console.log(username);
+    socket.emit('hello_user', 'hello' + username);
   }
 }
