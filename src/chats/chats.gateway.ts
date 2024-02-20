3a569e6594c068a9e0530c6b34a281f19e4095e8
김명식 <rlaaudtlr233@naver.com>
2024-02-20 18:00:16 +0900

    웹소켓 게이트웨이 라이프사이클 공부

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,13 +1,38 @@
+import { Logger } from '@nestjs/common';
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
 import { Socket } from 'dgram';
 
 @WebSocketGateway({ namespace: 'chattings' })
-export class ChatsGateway {
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  constructor() {
+    this.logger.log('constructor');
+  }
+
+  // afterInit() => 게이트웨이가 실행이될때 생성자(constructor) 다음으로 가장먼저 실행되는 메서드
+  afterInit() {
+    this.logger.log('init');
+  }
+  // handleConnection => 소켓이 커넥트 되면 실행되는 메서드
+  handleConnection(@ConnectedSocket() socket: any) {
+    this.logger.log(`connection : ${socket.id} ${socket.nsp.name}`);
+  }
+  // handleDiscconnect => 소켓 연결이 끊긴 직후 실행되는 메서드
+  handleDisconnect(@ConnectedSocket() socket: any) {
+    this.logger.log(`connection : ${socket.id} ${socket.nsp.name}`);
+  }
+
   @SubscribeMessage('new_user') // 소켓을 통해 주고받을 이벤트명을 기입
   handleNewUser(
     @MessageBody() username: string,
