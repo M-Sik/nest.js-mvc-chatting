1a92eefdfe22de0bfffe96fa3a1d7264f13f6f42
김명식 <rlaaudtlr233@naver.com>
2024-02-26 18:55:45 +0900

    socket 스키마 작성

--- src/chats/models/socket.model.ts
+++ src/chats/models/socket.model.ts
@@ -1,12 +1,29 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { Document, SchemaOptions } from 'mongoose';
 
 const options: SchemaOptions = {
+  id: false, // 몽구스에서 자동으로 생성하는 id가 아닌 소켓의 고유 id를 넣어주기 위함
   collection: 'sockets',
   timestamps: true, // updateAt, createdAt을 자동으로 찍어줌
 };
 
 @Schema(options)
-export class Socket extends Document {}
+export class Socket extends Document {
+  @Prop({
+    unique: true,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+
+  @Prop({
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+}
 
 export const SocketSchema = SchemaFactory.createForClass(Socket);
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -9,6 +10,8 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+  // class validation을 사용하기위해 등록 ex) @IsNotEmpty, @IsString 등등
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(8000);
 }
