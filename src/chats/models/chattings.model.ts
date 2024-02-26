fc81ae4c95e3a87edeb4b517eeb448ddf35cdd62
김명식 <rlaaudtlr233@naver.com>
2024-02-26 19:05:22 +0900

    chatting 스키마 작성 및 chat 모듈에 스키마 추가

--- src/chats/models/chattings.model.ts
+++ src/chats/models/chattings.model.ts
@@ -1,5 +1,7 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document, SchemaOptions } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Document, SchemaOptions, Types } from 'mongoose';
+import { Socket as SocketModel } from './socket.model';
 
 const options: SchemaOptions = {
   collection: 'chattings',
@@ -7,6 +9,23 @@ const options: SchemaOptions = {
 };
 
 @Schema(options)
-export class Chatting extends Document {}
+export class Chatting extends Document {
+  @Prop({
+    type: {
+      _id: { type: Types.ObjectId, required: true, ref: 'sockets' }, // 몽구스에서 자동으로 만들어주는 _id이며 sockets 스키마를 참조
+      id: { type: String }, // 소켓의 고유 id
+      username: { type: String, required: true },
+    },
+  })
+  @IsNotEmpty()
+  user: SocketModel;
+
+  @Prop({
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  chat: string;
+}
 
 export const ChattingSchema = SchemaFactory.createForClass(Chatting);
